2b36064a11c8b9d814766bc50181377b518791b8
Lance Lee <128726254+ifgforever@users.noreply.github.com>
2025-12-25 17:59:54 -0600

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,1 +1,11 @@
+export async function onRequest(context) {
+  const res = await context.next();
+  const out = new Response(res.body, res);
+
+  out.headers.set("X-Content-Type-Options", "nosniff");
+  out.headers.set("X-Frame-Options", "DENY");
+  out.headers.set("Referrer-Policy", "strict-origin-when-cross-origin");
+
+  return out;
+}
 
