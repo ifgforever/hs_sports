11ee663f4107f3560c63c28b7638336eef5f0943
Lance Lee <128726254+ifgforever@users.noreply.github.com>
2025-12-26 00:38:00 -0600

    Update logout.js

--- functions/api/auth/logout.js
+++ functions/api/auth/logout.js
@@ -1,5 +1,5 @@
 import { json, clearAuthCookie } from "../_util.js";
 
-export async function onRequestPost() {
-  return json({ ok: true }, { headers: clearAuthCookie() });
+export async function onRequestPost({ env }) {
+  return json({ ok: true }, { headers: clearAuthCookie(env) });
 }
